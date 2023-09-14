27d2c5e221f1da3a9d0d0eea4fd34c589563dcb3
AgostinaCipolla <133062972+AgostinaCipolla@users.noreply.github.com>
2023-09-14 11:19:18 -0300

    Add files via upload

---
+++ tp3/tp3/index.php
@@ -0,0 +1,53 @@
+<?php
+     echo "<b>Hola Mundo!</b>";
+     echo "<br>";
+     $asignar = "<b>Hola Mundo!</b>";
+     echo $asignar;
+     echo "<br>";echo "<br>";
+     $numero1 = 25;
+     $numero2 = 10;
+     //Suma
+     echo "La suma $numero1 + $numero2  es = ";
+     echo $numero1 + $numero2;
+     echo "<br>";
+     //Resta
+     echo "La resta $numero1 - $numero2  es = ";
+     echo $numero1 - $numero2;
+     echo "<br>";
+     //Multiplicacion
+     echo "La multiplicacion $numero1 * $numero2  es = ";
+     echo $numero1 * $numero2;
+     echo "<br>";
+     //Division entera
+     echo "La division $numero1 / $numero2  es = ";
+     echo $numero1 / $numero2;
+     echo "<br>";
+     //Resto de la division entera
+     echo "El resto de la division entera de $numero1 dividido $numero2  es = ";
+     echo $numero1 % $numero2;
+     echo "<br>";
+     //Conversion de grados celsius a fahrenheit
+     $gradosfahrenheit = (20*1.8) + 32;
+     echo "20° celsius equivalen a ";
+     echo $gradosfahrenheit; echo " grados fahrenheit";
+echo "<br>";echo "<br>";echo "<br>";
+
+     //Perímetro y area de un rectangulo con base de 18cm y altura 12cm.
+     $perimrectangulo = 2*(18+12);
+     echo "El perimetro de un rectangulo con base de 18cm y altura 12cm es = ";
+     echo $perimrectangulo; echo "cm";
+echo "<br>";
+     $arearectangulo = 18*12;
+     echo "El area de un rectangulo con base de 18cm y altura 12cm es = ";
+     echo $arearectangulo; echo "cm";
+echo "<br>";echo "<br>";
+     //Perímetro y área de un círculo de radio 30cm
+     $perimcirculo = 2*3.14*30;
+     echo "El perimetro de un circulo de radio 30cm es = ";
+     echo $perimcirculo; echo "cm";
+     echo "<br>";
+     $areacirculo = 3.14*30*30;
+     echo "El perimetro de un circulo de radio 30cm es = ";
+     echo $areacirculo; echo "cm";
+
+?>
